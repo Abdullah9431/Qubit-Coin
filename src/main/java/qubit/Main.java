4849a72a0fc280a615ba657dab2591e00cb39313
Abdullah9431 <abdaraz10@gmail.com>
2024-08-27 23:03:27 +0200

    minor change

--- src/main/java/qubit/Main.java
+++ src/main/java/qubit/Main.java
@@ -5,12 +5,11 @@ import ch.qos.logback.classic.Logger;
 
 public class Main {
     static final Logger logger = (Logger) LoggerFactory.getLogger(Main.class);
-    static String sender = "ikhaabcs";
-    static String receiver = "kjkhaabcs";
+    static String sender = "aaa";
+    static String receiver = "bbb";
     static String amount = "100";
 
     public static void main(String[] args) throws Exception {
-        Loggertils.SetLogLevel("debug");
         if(logger.isDebugEnabled()){
             logger.debug("Starting application with debug: true");
             Main.logger.debug("Running as user: " + System.getProperty("user.name"));
